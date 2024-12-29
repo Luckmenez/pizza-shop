62ff01b520cf6cf6c8044f61113025d4f92d911f
Lucas Menezes <luckmenez@gmail.com>
2024-12-29 19:11:13 -0300

    feat: implementado helmet da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './global.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router'
 
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+    </HelmetProvider>
 }
