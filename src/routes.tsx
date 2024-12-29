19baf3864a47fd970a03bc8a9be13720a47229c3
Lucas Menezes <luckmenez@gmail.com>
2024-12-29 19:16:45 -0300

    feat: criado rotas para componentes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,31 @@
 import { createBrowserRouter } from 'react-router'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { DashBoard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <DashBoard />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <DashBoard />,
+      },
+    ],
   },
   {
-    path: '/sign-in',
-    element: <SignIn />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
+    ],
   },
 ])
