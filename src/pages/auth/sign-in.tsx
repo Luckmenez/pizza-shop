55b86fffb7e55a569c60d16cff53a6505ab846dc
Lucas Menezes <luckmenez@gmail.com>
2025-03-04 18:20:00 -0300

    feat: implementada chamada para sign in

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useSearchParams } from 'react-router'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -28,15 +30,18 @@ export function SignIn() {
     },
   })
 
-  async function handleSignIn(data: SignInForm) {
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
+  async function handleSignIn({ email }: SignInForm) {
     try {
-      console.log(data)
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await authenticate({ email })
 
       toast.success('Enviamos um link de autenticação para o seu e-mail.', {
         action: {
           label: 'Reenviar',
-          onClick: () => handleSignIn(data),
+          onClick: () => handleSignIn({ email }),
         },
       })
     } catch (error) {
