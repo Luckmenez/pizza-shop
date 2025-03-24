56204b2e5a3bce2b3f981c19f1196179070fafbd
Lucas Menezes <luckmenez@gmail.com>
2025-03-23 22:15:16 -0300

    feat: implementado formulário de filtros

--- src/pages/app/orders/order-table-filters.tsx
+++ src/pages/app/orders/order-table-filters.tsx
@@ -79,30 +79,58 @@ export function OrderTableFilters() {
   }
 
   return (
-    <form className="flex items-center gap-2">
+    <form
+      onSubmit={handleSubmit(handleFilter)}
+      className="flex items-center gap-2"
+    >
       <span className="text-sm font-semibold">Filtors:</span>
-      <Input placeholder="Id do pedido" className="h-8 w-[320px]" />
-      <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-      <Select>
-        <SelectTrigger className="h-8 w-[180px]">
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent defaultValue="all">
-          <SelectItem value="all">Todos status</SelectItem>
-          <SelectItem value="pending">Pendente</SelectItem>
-          <SelectItem value="canceled">Cancelado</SelectItem>
-          <SelectItem value="processing">Em preparo</SelectItem>
-          <SelectItem value="delivering">Em entrega</SelectItem>
-          <SelectItem value="delivered">Entregue</SelectItem>
-        </SelectContent>
-      </Select>
+      <Input
+        placeholder="Id do pedido"
+        className="h-8 w-[320px]"
+        {...register('orderId')}
+      />
+      <Input
+        placeholder="Nome do cliente"
+        className="h-8 w-[320px]"
+        {...register('customerName')}
+      />
+      <Controller
+        name="status"
+        control={control}
+        render={({ field: { name, onChange, value, disabled } }) => (
+          <Select
+            defaultValue="all"
+            name={name}
+            onValueChange={onChange}
+            value={value}
+            disabled={disabled}
+          >
+            <SelectTrigger className="h-8 w-[180px]">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent defaultValue="all">
+              <SelectItem value="all">Todos status</SelectItem>
+              <SelectItem value="pending">Pendente</SelectItem>
+              <SelectItem value="canceled">Cancelado</SelectItem>
+              <SelectItem value="processing">Em preparo</SelectItem>
+              <SelectItem value="delivering">Em entrega</SelectItem>
+              <SelectItem value="delivered">Entregue</SelectItem>
+            </SelectContent>
+          </Select>
+        )}
+      />
 
       <Button type="submit" variant={'secondary'} size={'xs'}>
         <Search className="mr-2 h-4 w-4" />
         Filtrar resultados
       </Button>
 
-      <Button type="button" variant={'outline'} size={'xs'}>
+      <Button
+        onClick={handleClearFilters}
+        type="button"
+        variant={'outline'}
+        size={'xs'}
+      >
         <X className="mr-2 h-4 w-4" />
         Remover Filtros
       </Button>
