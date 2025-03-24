8fe3ec89b0ab5b5fdfef75c46268850af28e4e10
Lucas Menezes <luckmenez@gmail.com>
2025-03-23 22:05:05 -0300

    feat: implementado busca de dados de cancelados por mes

--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,8 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
 import { DollarSign } from 'lucide-react'
 
+import { getMonthCanceledOrdersAmount } from '@/api/get-month-canceled-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function MonthCanceledOrdersAmountCard() {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryKey: ['metrics', 'month-canceled-orders-amount'],
+    queryFn: getMonthCanceledOrdersAmount,
+  })
   return (
     <Card className="bg-background">
       <CardHeader className="flex-row items-center justify-between space-y-0">
